f3852373c9b9d21a4c7789ffda17a0b85fdacba1
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-01-23 20:44:29 -0300

    Teste

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 const router = express.Router();
+// import { UsuarioController } from '../adapter/driver/UsuarioController';
+
 
 router.get('/', (req, res) => {
   res.status(200).send('OK');
 });
 
+
+router.post('/autenticaCliente', async (req, res) => {
+  let cpf = req.body.cpf;
+  if (!cpf) {
+    return res.status(400).json({ error: 'CPF inválido' });
+  }
+  // let usuarioController = new UsuarioController();
+  // await usuarioController.autenticaCliente(cpf, res);
+});
 
 export default router;
