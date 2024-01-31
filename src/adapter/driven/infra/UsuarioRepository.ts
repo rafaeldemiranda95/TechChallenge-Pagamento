94e114351487408469457e2f95408c6f53a883ab
AirtonPatrocinioJR <airtonpatrocinio.jr@gmail.com>
2024-01-31 20:04:46 -0300

    ajustes testes

--- src/adapter/driven/infra/UsuarioRepository.ts
+++ src/adapter/driven/infra/UsuarioRepository.ts
@@ -8,11 +8,6 @@ export class UsuarioRepository implements IUsuarioUseCase {
   async validarToken(token: string): Promise<boolean | undefined> {
     let query = `select * from public.usuario where token = '${token}'`;
     let _getUsuarioDb = await runQuery(query);
-    // let getUsuarioDb = await prisma.usuario.findUnique({
-    //   where: {
-    //     token: token,
-    //   },
-    // });
     if (_getUsuarioDb.length > 0) {
       return true;
     } else {
@@ -20,50 +15,39 @@ export class UsuarioRepository implements IUsuarioUseCase {
     }
   }
 
-  // Manter
   async autenticaAdministrador(usuario: Usuario): Promise<string | undefined> {
-    // let getUsuarioDb = await prisma.usuario.findUnique({
-    //   where: {
-    //     email: usuario.email,
-    //   },
-    // });
-
-    let query = `select * from public.usuario where email = '${usuario.email}'`;
-    let _getUsuarioDb = await runQuery(query);
-    console.log(_getUsuarioDb);
-
-    if (_getUsuarioDb.length > 0) {
-      let getUsuarioDb = _getUsuarioDb[0];
-      if (usuario.senha != undefined && getUsuarioDb.senha != undefined) {
-        let validaSenha = new VerificaSenha();
-        if (validaSenha) {
-          let token = jwt.sign(
-            {
-              id: getUsuarioDb.id,
-              nome: getUsuarioDb.nome,
-              email: getUsuarioDb.email,
-              tipo: getUsuarioDb.tipo,
-            },
-            process.env.JWT_SECRET,
-            {
-              expiresIn: '365d',
-            }
-          );
-          // await prisma.usuario.update({
-          //   where: { id: getUsuarioDb.id },
-          //   data: { token: token },
-          // });
-
-          let saveQuery = `UPDATE public.usuario SET token = '${token}' where id = ${getUsuarioDb.id};`;
-          await runQuery(saveQuery);
-          return token;
+    try {
+      let query = `select * from public.usuario where email = '${usuario.email}'`;
+      let _getUsuarioDb = await runQuery(query);
+      if (_getUsuarioDb.length > 0) {
+        let getUsuarioDb = _getUsuarioDb[0];
+        console.log('getUsuarioDb   ===>>>  ', getUsuarioDb);
+        console.log('usuario.senha   ===>>>  ', getUsuarioDb.senha);
+        if (usuario.senha != undefined && getUsuarioDb.senha != undefined) {
+          let validaSenha = new VerificaSenha();
+          if (validaSenha) {
+            let token = jwt.sign(
+              {
+                id: getUsuarioDb.id,
+                nome: getUsuarioDb.nome,
+                email: getUsuarioDb.email,
+                tipo: getUsuarioDb.tipo,
+              },
+              process.env.JWT_SECRET,
+              {
+                expiresIn: '365d',
+              }
+            );
+            let saveQuery = `UPDATE public.usuario SET token = '${token}' where id = ${getUsuarioDb.id};`;
+            await runQuery(saveQuery);
+            return token;
+          }
         }
-      } else {
         return undefined;
       }
+    } catch (err: any) {
+      throw err;
     }
-
-    return undefined;
   }
 
   async autenticaCliente(usuario: Usuario): Promise<string | undefined> {
@@ -90,7 +74,104 @@ export class UsuarioRepository implements IUsuarioUseCase {
         return token;
       }
     } catch (error: any) {
-      throw new Error(error);
+      throw error;
     }
   }
 }
+
+// import { runQuery } from '../../../config/database';
+// import { Usuario } from '../../../core/domain/models/Usuario';
+// import { IUsuarioUseCase } from '../../../core/domain/useCases/Usuario/IUsuarioUseCase';
+// import { VerificaSenha } from '../../../core/domain/valueObjects/VerificaSenha';
+// var jwt = require('jsonwebtoken');
+
+// export class UsuarioRepository implements IUsuarioUseCase {
+//   async validarToken(token: string): Promise<boolean | undefined> {
+//     let query = `select * from public.usuario where token = '${token}'`;
+//     let _getUsuarioDb = await runQuery(query);
+//     // let getUsuarioDb = await prisma.usuario.findUnique({
+//     //   where: {
+//     //     token: token,
+//     //   },
+//     // });
+//     if (_getUsuarioDb.length > 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   // Manter
+//   async autenticaAdministrador(usuario: Usuario): Promise<string | undefined> {
+//     // let getUsuarioDb = await prisma.usuario.findUnique({
+//     //   where: {
+//     //     email: usuario.email,
+//     //   },
+//     // });
+
+//     let query = `select * from public.usuario where email = '${usuario.email}'`;
+//     let _getUsuarioDb = await runQuery(query);
+//     console.log(_getUsuarioDb);
+
+//     if (_getUsuarioDb.length > 0) {
+//       let getUsuarioDb = _getUsuarioDb[0];
+//       if (usuario.senha != undefined && getUsuarioDb.senha != undefined) {
+//         let validaSenha = new VerificaSenha();
+//         if (validaSenha) {
+//           let token = jwt.sign(
+//             {
+//               id: getUsuarioDb.id,
+//               nome: getUsuarioDb.nome,
+//               email: getUsuarioDb.email,
+//               tipo: getUsuarioDb.tipo,
+//             },
+//             process.env.JWT_SECRET,
+//             {
+//               expiresIn: '365d',
+//             }
+//           );
+//           // await prisma.usuario.update({
+//           //   where: { id: getUsuarioDb.id },
+//           //   data: { token: token },
+//           // });
+
+//           let saveQuery = `UPDATE public.usuario SET token = '${token}' where id = ${getUsuarioDb.id};`;
+//           await runQuery(saveQuery);
+//           return token;
+//         }
+//       } else {
+//         return undefined;
+//       }
+//     }
+
+//     return undefined;
+//   }
+
+//   async autenticaCliente(usuario: Usuario): Promise<string | undefined> {
+//     try {
+//       let query = `select * from public.usuario where cpf = '${usuario.cpf}'`;
+//       let _getUsuarioDb = await runQuery(query);
+//       if (_getUsuarioDb.length > 0) {
+//         let getUsuarioDb = _getUsuarioDb[0];
+//         let token = jwt.sign(
+//           {
+//             id: getUsuarioDb.id,
+//             nome: getUsuarioDb.nome,
+//             email: getUsuarioDb.email,
+//             tipo: getUsuarioDb.tipo,
+//             cpf: getUsuarioDb.cpf,
+//           },
+//           process.env.JWT_SECRET,
+//           {
+//             expiresIn: '365d',
+//           }
+//         );
+//         let saveQuery = `UPDATE public.usuario SET token = '${token}' where id = ${getUsuarioDb.id};`;
+//         await runQuery(saveQuery);
+//         return token;
+//       }
+//     } catch (error: any) {
+//       throw new Error(error);
+//     }
+//   }
+// }
